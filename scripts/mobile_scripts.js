521b937f2316df7b0d145ca7723b32681b546dd0
Rick <nikitalternative95@gmail.com>
2020-09-29 16:25:54 +0500

    mobile 4

--- scripts/mobile_scripts.js
+++ scripts/mobile_scripts.js
@@ -1,3 +1,12 @@
-(()=>{
-	console.log(1);
-})()
+var header;
+
+window.addEventListener("load", () => {
+	header = document.querySelector("header");
+		nav = document.querySelector("header > .navigation")
+
+	console.log(nav)
+	header.addEventListener("click", () => {
+		console.log(1);
+		nav.classList.toggle("active");
+	})
+})
