32ab077c8aeacc4b4eafab1a465833ceb1090e91
morty_smth <morti.show@gmail.com>
2020-11-12 13:30:40 +0500

    dsdh

--- admin.php
+++ admin.php
@@ -155,8 +155,10 @@
 
 				$objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
 	 
-				header('Content-Disposition: attachment; filename="report.docx"');
-				// header('Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+				header("Cache-Control: public");
+header("Content-Description: File Transfer");
+header("Content-Disposition: attachment; filename=ffff.docx");
+header("Content-Transfer-Encoding: binary");  
 				
 				ob_clean();
 				$objWriter->save('php://output');
